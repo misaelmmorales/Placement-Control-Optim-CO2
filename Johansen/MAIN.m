35f520ac8b287f9dec3f454a9c95516366cc01b2
Misael M. Morales <misaelmiguelmorales@gmail.com>
2024-03-15 22:28:36 -0500

    johansen

--- Johansen/MAIN.m
+++ Johansen/MAIN.m
@@ -1,49 +1,58 @@
+clear;clc;close all
 %% Main variables
-clear; close all; clc
 set(0,'DefaultFigureWindowStyle','docked')
 
+proj_dir = 'E:/Placement-Control-Optim-CO2';
+mdir = 'C:/Users/Misael Morales/OneDrive - The University of Texas at Austin/Documents/MATLAB/mrst-2022b';
+chdir(mdir);
+startup;
+chdir(proj_dir);
+
 % Import MRST module
-mrstModule add SPE10 co2lab mrst-gui
-mrstModule add ad-core ad-props ad-blackoil
+mrstModule add spe10 co2lab ad-core ad-props ad-blackoil mrst-gui
 
-%% Make Grid
-dims = 32;
-nx=dims; ny=dims; nz=1; 
-dx=1000*meter; dy=1000*meter; dz=100*meter;
+%% Make Johansen Grid
 
-% Make cartesian grid
-G = cartGrid([nx ny nz], [dx dy dz]);
+dpath = getDatasetPath('johansen');
+sector = fullfile(dpath, 'NPD5');
+filename = [sector, '.grdecl'];
+
+grdecl = readGRDECL(filename);  clear filename
+grdecl.ACTNUM = grdecl.ACTNUM;
+G = processGRDECL(grdecl);
 G = computeGeometry(G);
 
-%% Make Rock
-logperm = readmatrix('perm_realization.csv');
-random = 0.2*randn([dims*dims,1]);
+nx = G.cartDims(1);
+ny = G.cartDims(2);
+nz = G.cartDims(3);
 
-poro = 10.^((logperm-7)/10);
-permx = (10.^(0.25+logperm+random))*milli*darcy;
-permy = permx;
-permz = 0.1*permx;
-perm = [permx, permy, permz];
+%% Ground Truth model
+
+p = reshape(load([sector, '_Porosity.txt'])', prod(G.cartDims), []);
+poro = p(G.cells.indexMap); clear p
+
+K = reshape(load([sector, '_Permeability.txt']')', prod(G.cartDims), []);
+perm = bsxfun(@times, [1 1 0.1], K(G.cells.indexMap)).*milli*darcy; clear K;
 rock = makeRock(G, perm, poro);
-permeability = convertTo(perm, milli*darcy);
+
+w = load([sector, '_Well.txt']);
 
 %% Make Initial State
-gravity on;  g = gravity;
-rhow = 1000; % density of brine corresponding to 94 degrees C and 300 bar
-%initState.pressure = G.cells.centroids(:,3) * 400*psia; %G.cells.centroids=3
-P0 = 4000*psia; 
-initState.pressure = repmat(P0, 1024,1);
+gravity on;
+g = gravity;
+rhow = 1000;
+initState.pressure = rhow * g(3) * G.cells.centroids(:,3);
 initState.s = repmat([1, 0], G.cells.num, 1);
 initState.sGmax = initState.s(:,2);
 
 %% Make Fluid
-co2     = CO2props();             % load sampled tables of co2 fluid properties
-p_ref   = 30 * mega * Pascal;     % choose reference pressure
-t_ref   = 94 + 273.15;            % choose reference temperature, in Kelvin
-rhoc    = co2.rho(p_ref, t_ref);  % co2 density at ref. press/temp
+co2     = CO2props(); % load sampled tables of co2 fluid properties
+p_ref   = 15 * mega * Pascal; % choose reference pressure
+t_ref   = 70 + 273.15; % choose reference temperature, in Kelvin
+rhoc    = co2.rho(p_ref, t_ref); % co2 density at ref. press/temp
 cf_co2  = co2.rhoDP(p_ref, t_ref) / rhoc; % co2 compressibility
-cf_wat  = 0;                      % brine compressibility (zero)
-cf_rock = 4.35e-5 / barsa;        % rock compressibility
+cf_wat  = 0; % brine compressibility (zero)
+cf_rock = 4.35e-5 / barsa; % rock compressibility
 muw     = 8e-4 * Pascal * second; % brine viscosity
 muco2   = co2.mu(p_ref, t_ref) * Pascal * second; % co2 viscosity
 
@@ -56,16 +65,16 @@ fluid = initSimpleADIFluid('phases', 'WG'           , ...
                            'cR'  , cf_rock          , ...
                            'n'   , [2 2]);
 
-% Modify relative permeability curves
+% Change relperm curves
 srw = 0.27;
 src = 0.20;
 fluid.krW = @(s) fluid.krW(max((s-srw)./(1-srw), 0));
 fluid.krG = @(s) fluid.krG(max((s-src)./(1-src), 0));
 
-% Add capillary pressure
+% Add capillary pressure curve
 pe = 5 * kilo * Pascal;
 pcWG = @(sw) pe * sw.^(-1/2);
-fluid.pcWG = @(sg) pcWG(max((1-sg-srw)./(1-srw), 1e-5)); 
+fluid.pcWG = @(sg) pcWG(max((1-sg-srw)./(1-srw), 1e-5));
 
 %% Make Boundary Conditions
 bc = [];
@@ -76,79 +85,73 @@ bc_cell_ix = sum(G.faces.neighbors(bc_face_ix, :), 2);
 p_face_pressure = initState.pressure(bc_cell_ix);
 bc = addBC(bc, bc_face_ix, 'pressure', p_face_pressure, 'sat', [1,0]);
 
-%% Define Timesteps
-inj_time = 10*year;
-mon_time = 90*year;
+%% Define timesteps
+dT1 = rampupTimesteps(10*year,year/2,0);  
+dT2 = rampupTimesteps(100*year, 5*year, 0);
+dT = [dT1; dT2];
+dTinj = sum(dT1)/year;
 
-inj_timestep = rampupTimesteps(inj_time, year/12, 0);
-inj_cumtime  = convertTo(cumsum(inj_timestep), year);
+%% Ground Truth simulation
+irate = 50 * mega * 1e3 / year / rhoc;
+maxrate = 10 * mega * 1e3 / year / rhoc;
 
-mon_timestep = rampupTimesteps(mon_time, year, 0);
-mon_cumtime  = convertTo(cumsum(mon_timestep), year);
+W = verticalWell([], G, rock,  w(1), w(2), w(3):w(4),  ...
+                 'InnerProduct', 'ip_tpf', ...
+                 'sign', 1,...
+                 'Radius', 0.1, ...
+                 'name', 'I',...
+                 'comp_i', [0,1], ...
+                 'type', 'rate', ...
+                 'Val', irate);
 
-total_timestep = [inj_timestep; mon_timestep]; 
-total_cumtime  = [inj_cumtime;  mon_cumtime];
+schedule.control    = struct('W', W, 'bc', bc);
+schedule.control(2) = struct('W', W, 'bc', bc);
+schedule.control(2).W.val = 0;
 
-%% Run Simulations
-N_realization = 2;
+schedule.step.val = [dT1; dT2];
+schedule.step.control = [ones(numel(dT1),1); ones(numel(dT2),1)*2];
 
-well_locations = cell(N_realization,1);
-results = cell(1,N_realization);
+model = TwoPhaseWaterGasModel(G, rock, fluid, 0, 0);
+[wellSol, states] = simulateScheduleAD(initState, model, schedule);
 
-parfor i=1:N_realization
-    inj_loc = randi([4,28],[1,2]);
-    W = gen_wells(G, rock, inj_loc, irate);
-    [schedule, dT1] = gen_schedule(W, bc, timestep);
+%%
+figure; plotToolbar(G,states); plotWell(G,W); colormap jet; colorbar; view(-145,60)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%% Generate Models & Run Simulation
+N = 1000;
+M = size(total_time,1);
+
+perm = load('perm_64x64.mat').perm';
+poro = load('poro_64x64.mat').poro';
+facies = load('facies_64x64.mat').facies';
+
+parfor i=1:N
+    nwells                   = randi([1,3], 1);
+    well_loc                 = randi([16,48], nwells, 2);
+    rock                     = gen_rock(i, perm, poro, facies);
+    W                        = gen_wells(G, rock, well_loc);
+    [schedule, dT1]          = gen_schedule(W, bc, timestep1);
     [model, wellSol, states] = gen_simulation(G, rock, fluid, initState, schedule);
-    well_locations{i} = inj_loc;
-    results{i} = states;
+
+    wname = sprintf('wells/wells%d', i);   parsave(wname, well_loc)
+    rname = sprintf('states/states%d', i); parsave(rname, states)
+    fprintf('Simulation %i done\n', i)
 end
 
-%% Collect Results
-well_locations = cell2mat(well_locations);
-
-pressure = zeros(N_realization,dims*dims,length(timestep));
-saturation = zeros(N_realization,dims*dims,length(timestep));
-bhp = zeros(N_realization,length(timestep));
-
-for i=1:N_realization
-    for j=1:length(timestep)
-        pressure(i,:,j) = convertTo(results{1,i}{j,1}.pressure, psia);
-        saturation(i,:,j) = results{1,i}{j,1}.s(:,2);
-        bhp(i,j) = convertTo(results{1,i}{j,1}.wellSol.bhp, psia);
-    end
-end
-
-%% Save
-%{
-save('permeability.mat', 'permeability')
-save('porosity.mat', 'poro')
-save('time_yr.mat', 'cum_time')
-save('results.mat', 'results')
-save('well_locations.mat', 'well_locations')
-save('pressure.mat','pressure')
-save('saturation.mat','saturation')
-save('bhp.mat', 'bhp')
-%}
-
-%% Plots
-%{
-figure; plotToolbar(G, results{1}); colormap jet
-
-figure; 
-plotCellData(G, rock.poro); 
-plotWell(G, W, 'color', 'k');
-title('Porosity & Well Locations'); xlabel('X [m]'); ylabel('Y [m]')
-view(-50,85); colormap jet; h=colorbar; h.Label.String = 'Porosity [v/v]';
-
-figure; plotCellData(G, states{1}.s(:,1)); colormap jet; view(-50,85)
-figure; plotCellData(G, states{end}.s(:,1)); colormap jet; view(-50,85)
-
-bhp_inj = zeros(length(timestep),1);
-for i=1:length(timestep)
-    bhp_inj(i) = convertTo(states{i}.wellSol(1).bhp, psia);
-end
-figure; plot(cum_time, bhp_inj, 'DisplayName','Injector');
-legend; title('BHP vs. Time'); xlabel('Time [years]'); ylabel('BHP [psia]')
-%}
 %% END
--- Johansen/make_rock_layers.m
+++
@@ -1,9 +0,0 @@
-function [poro, perm] = make_rock_layers(startlayer,endlayer)
-
-    rock = getSPE10rock(startlayer:endlayer);
-    nz = endlayer - startlayer + 1;
-    poro = reshape(rock.poro, [60,220, nz]);
-    perm = reshape(rock.perm(:,1), [60, 220, nz]);
-
-end
-
