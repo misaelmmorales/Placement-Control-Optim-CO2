ab28a5bd47f296f67c7cee65c4cb6ba69ef9dbe0
Misael M. Morales <misaelmiguelmorales@gmail.com>
2024-06-17 01:28:30 -0500

    3d update

--- gen_schedule.m
+++ gen_schedule.m
@@ -4,23 +4,28 @@ function [schedule] = gen_schedule(W, bc, fluid)
 
     well_rate = 5*rand(num_wells,20);
     well_rate(well_rate < 0.5) = 0;
-    well_rate = well_rate * (100*num_wells / sum(well_rate,'all'));
+    well_rate = well_rate * (7.5*10*num_wells / sum(well_rate,'all'));
     well_rate = well_rate * 1e3 * mega / year / fluid.rhoGS;
 
-    for i=1:20
+    rampsteps = rampupTimesteps(year/2, year/2, 9);
+
+    for i=1:30
         schedule.control(i) = struct('W', W, 'bc', bc);
     end
-    schedule.control(21) = struct('W', W, 'bc', bc);
+    schedule.control(31) = struct('W', W, 'bc', bc);
 
     for i=1:num_wells
-        schedule.control(21).W(i).val = 0;
-        for k=1:20
-            schedule.control(k).W(i).val = well_rate(i,k);
+        schedule.control(31).W(i).val = 0;
+        for k=1:10
+            schedule.control(k).W(i).val = well_rate(i,1);
+        end
+        for k=2:10
+            schedule.control(k+9).W(i).val = well_rate(i,k);
         end
     end
 
-    schedule.step.val     = [repmat(year/2,20,1); repmat(100*year,10,1)];
-    schedule.step.control = [linspace(1,20,20)'; ones(10,1)*21];
+    schedule.step.val     = [rampsteps;  repmat(year/2,19,1); repmat(100*year,10,1)];
+    schedule.step.control = [ones(10,1); linspace(2,20,19)';  ones(10,1)*21];
 
 end
 
--- gen_simulation.m
+++ gen_simulation.m
@@ -1,6 +1,8 @@
 function [wellSol, states] = gen_simulation(G, rock, fluid, initState, schedule)
     %% Simulation
+    lsolve = BackslashSolverAD();
     model = TwoPhaseWaterGasModel(G, rock, fluid, 0, 0);
-    [wellSol, states] = simulateScheduleAD(initState, model, schedule);
+    [wellSol, states] = simulateScheduleAD(initState, model, schedule, ...
+                                           'LinearSolver', lsolve);
 
 end
